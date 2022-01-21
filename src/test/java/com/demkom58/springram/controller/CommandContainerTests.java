d36b49b31bde1bddcdb59e2f8f87712f3dd9b26b
Max Demydenko <demkom58@gmail.com>
2022-01-22 01:27:26 +0200

    ref: update tests according to chains refactoring

--- src/test/java/com/demkom58/springram/controller/CommandContainerTests.java
+++ src/test/java/com/demkom58/springram/controller/CommandContainerTests.java
@@ -4,6 +4,7 @@ import com.demkom58.springram.controller.annotation.BotController;
 import com.demkom58.springram.controller.annotation.CommandMapping;
 import com.demkom58.springram.controller.annotation.PathVariable;
 import com.demkom58.springram.controller.container.CommandContainer;
+import com.demkom58.springram.controller.container.PathAlreadyTakenException;
 import com.demkom58.springram.controller.message.MessageType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,10 +12,24 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
+import java.lang.reflect.Method;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith({MockitoExtension.class})
 class CommandContainerTests {
+    private static Method testControllerMethod;
+    private static Method patternControllerMethod;
+
+    static {
+        try {
+            testControllerMethod = TestController.class.getDeclaredMethod("test", String.class);
+            patternControllerMethod = PatternFirstTestController.class.getDeclaredMethod("test", String.class);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
     private CommandContainer container;
 
     @BotController
@@ -41,18 +56,16 @@ class CommandContainerTests {
     @Test
     void addMethod_testController_success() throws Exception {
         final TestController testController = new TestController();
-        container.addMethod(testController, TestController.class.getDeclaredMethod("test", String.class));
+        container.addMethod(testController, testControllerMethod);
     }
 
     @Test
     void addMethod_doubleRegisterTestController_exception() throws Exception {
         final TestController testController = new TestController();
-        container.addMethod(testController, TestController.class.getDeclaredMethod("test", String.class));
+        container.addMethod(testController, testControllerMethod);
         assertThrows(
-                IllegalStateException.class,
-                () -> container.addMethod(testController,
-                        TestController.class.getDeclaredMethod("test", String.class)
-                )
+                PathAlreadyTakenException.class,
+                () -> container.addMethod(testController, testControllerMethod)
         );
     }
 
@@ -60,22 +73,20 @@ class CommandContainerTests {
     void addMethod_patternFirstTestController_exception() throws Exception {
         assertThrows(
                 IllegalArgumentException.class,
-                () -> container.addMethod(new PatternFirstTestController(),
-                        PatternFirstTestController.class.getDeclaredMethod("test", String.class)
-                )
+                () -> container.addMethod(new PatternFirstTestController(), patternControllerMethod)
         );
     }
 
     @Test
     void findControllers_getRegisteredMethod_success() throws Exception {
         final TestController testController = new TestController();
-        container.addMethod(testController, TestController.class.getDeclaredMethod("test", String.class));
-        assertNotNull(container.findHandler(MessageType.TEXT_MESSAGE, "default", "test test"));
+        container.addMethod(testController, testControllerMethod);
+        assertNotNull(container.findHandler(MessageType.TEXT_MESSAGE, null, "test test"));
     }
 
     @Test
     void findControllers_getNotRegisteredMethod_null() throws Exception {
-        assertNull(container.findHandler(MessageType.TEXT_MESSAGE, "default", "test test"));
+        assertNull(container.findHandler(MessageType.TEXT_MESSAGE, null, "test test"));
     }
 
 }
