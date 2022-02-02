ff81a956c121b35d1097ce1fcdcdf4724bf8e19d
Max Demydenko <demkom58@gmail.com>
2022-02-02 12:03:54 +0200

    fix: add support of all BotApiMethod messages

--- src/main/java/com/demkom58/springram/controller/method/result/impl/BotApiMethodHandlerMethodReturnValueHandler.java
+++ src/main/java/com/demkom58/springram/controller/method/result/impl/BotApiMethodHandlerMethodReturnValueHandler.java
@@ -1,21 +1,21 @@
 package com.demkom58.springram.controller.method.result.impl;
 
-import com.demkom58.springram.controller.method.result.HandlerMethodReturnValueHandler;
 import com.demkom58.springram.controller.message.TelegramMessage;
+import com.demkom58.springram.controller.method.result.HandlerMethodReturnValueHandler;
 import org.springframework.core.MethodParameter;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 
-public class SendMessageHandlerMethodReturnValueHandler implements HandlerMethodReturnValueHandler {
+public class BotApiMethodHandlerMethodReturnValueHandler implements HandlerMethodReturnValueHandler {
     @Override
     public boolean isSupported(MethodParameter returnType) {
-        return SendMessage.class.isAssignableFrom(returnType.getParameterType());
+        return BotApiMethod.class.isAssignableFrom(returnType.getParameterType());
     }
 
     @Override
     public void handle(MethodParameter returnType, TelegramMessage message, AbsSender bot, Object result)
             throws Exception {
-        final SendMessage sm = (SendMessage) result;
+        final BotApiMethod<?> sm = (BotApiMethod<?>) result;
         bot.execute(sm);
     }
 }
